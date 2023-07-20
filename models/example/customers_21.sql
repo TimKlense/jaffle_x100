2246972916818ce2608644ed86a26f9c6c45eb63
Tim Klense <timklense@yahoo.com>
2023-07-20 10:39:59 -0500

    move files over to models

---
+++ models/example/customers_21.sql
@@ -0,0 +1,34 @@
+
+with orders as (
+
+	select * from {{ ref('orders_21') }}
+
+),
+
+customers as (
+
+	select * from {{ ref('stg_customers_21') }}
+
+),
+
+final as (
+
+	select
+		customers.customer_id,
+		customers.first_name,
+		customers.last_name,
+
+		count(orders.order_id) as orders_count,
+
+		sum(orders.amount) as total_amount
+
+	from customers
+
+	left join orders
+		on customers.customer_id = orders.customer_id
+
+	group by 1, 2, 3
+
+)
+
+select * from final
---
+++ models/example/customers_57.sql
@@ -0,0 +1,34 @@
+
+with orders as (
+
+	select * from {{ ref('orders_57') }}
+
+),
+
+customers as (
+
+	select * from {{ ref('stg_customers_57') }}
+
+),
+
+final as (
+
+	select
+		customers.customer_id,
+		customers.first_name,
+		customers.last_name,
+
+		count(orders.order_id) as orders_count,
+
+		sum(orders.amount) as total_amount
+
+	from customers
+
+	left join orders
+		on customers.customer_id = orders.customer_id
+
+	group by 1, 2, 3
+
+)
+
+select * from final
